93b57aa7c69a0573a0e54e63bbe0fff82121ccd1
GedMarc <ged_marc@hotmail.com>
2018-08-10 13:30:06 +0200

    Plugin POM updates for JRE10, and maven dependency classification bug fix

--- src/main/java/com/jwebmp/plugins/globalize/cultures/GlobalizePageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/globalize/cultures/GlobalizePageConfigurator.java
@@ -20,6 +20,8 @@ import com.jwebmp.core.Page;
 import com.jwebmp.core.plugins.PluginInformation;
 import com.jwebmp.core.services.IPageConfigurator;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * @author GedMarc
  */
@@ -41,6 +43,7 @@ import com.jwebmp.core.services.IPageConfigurator;
 public class GlobalizePageConfigurator
 		implements IPageConfigurator
 {
+	@NotNull
 	@Override
 	public Page configure(Page page)
 	{

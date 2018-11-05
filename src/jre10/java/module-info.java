3f3a7e2d49f7f7f7a4a33a324d8fa68cfd9bafd6
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:40:19 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,5 +1,8 @@
 import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 import com.jwebmp.plugins.globalize.cultures.GlobalizePageConfigurator;
+import com.jwebmp.plugins.globalize.cultures.implementations.GlobalizeExclusionsModule;
 
 module com.jwebmp.plugins.globalize.cultures {
 	exports com.jwebmp.plugins.globalize.cultures;
@@ -10,7 +13,12 @@ module com.jwebmp.plugins.globalize.cultures {
 
 	requires java.validation;
 	requires java.logging;
+	requires com.jwebmp.guicedinjection;
 
 	provides IPageConfigurator with GlobalizePageConfigurator;
-	opens com.jwebmp.plugins.globalize.cultures to com.fasterxml.jackson.databind,com.jwebmp.core;
+
+	provides IGuiceScanJarExclusions with GlobalizeExclusionsModule;
+	provides IGuiceScanModuleExclusions with GlobalizeExclusionsModule;
+
+	opens com.jwebmp.plugins.globalize.cultures to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
---
+++ src/main/java/com/jwebmp/plugins/globalize/cultures/implementations/GlobalizeExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.globalize.cultures.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class GlobalizeExclusionsModule
+		implements IGuiceScanModuleExclusions<GlobalizeExclusionsModule>,
+				           IGuiceScanJarExclusions<GlobalizeExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-globalize-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.globalize.cultures");
+		return strings;
+	}
+}
